814e9863a666d306ca40bbc1d254658cf42043fb
Murphy Hsieh <murphyhsieh@leandev.io>
2020-08-24 16:42:13 +0800

    Setter Injection using java annotation, Autowired, Component

--- src/com/murphy/TennisCoach.java
+++ src/com/murphy/TennisCoach.java
@@ -8,9 +8,14 @@ public class TennisCoach implements Coach {
 
     private FortuneService fortuneService;
 
+    public TennisCoach() {
+        System.out.println("TennisCoach: inside default constructor");
+    }
+
     @Autowired
-    public TennisCoach(FortuneService theFortuneService) {
-        fortuneService = theFortuneService;
+    public void setFortuneService(FortuneService fortuneService) {
+        System.out.println("TennisCoach: inside setFortuneService() method");
+        this.fortuneService = fortuneService;
     }
 
     @Override
