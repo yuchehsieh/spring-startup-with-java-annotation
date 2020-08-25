1ae9d4ddee1932ed8334a5884f81c0f4b9835b12
Murphy Hsieh <murphyhsieh@leandev.io>
2020-08-25 08:27:56 +0800

    Field Injection using java annotation, Autowired, Component

--- src/com/murphy/TennisCoach.java
+++ src/com/murphy/TennisCoach.java
@@ -6,17 +6,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 
+    @Autowired
     private FortuneService fortuneService;
 
     public TennisCoach() {
         System.out.println("TennisCoach: inside default constructor");
     }
 
-    @Autowired
-    public void doSomeCrazyStuff(FortuneService fortuneService) {
-        System.out.println("TennisCoach: inside doSomeCrazyStuff() method");
-        this.fortuneService = fortuneService;
-    }
+//    @Autowired
+//    public void doSomeCrazyStuff(FortuneService fortuneService) {
+//        System.out.println("TennisCoach: inside doSomeCrazyStuff() method");
+//        this.fortuneService = fortuneService;
+//    }
 
     @Override
     public String getDailyWorkout() {
