d0ec236f5904995e90703148dca21fcd6b0d17be
Murphy Hsieh <murphyhsieh@leandev.io>
2020-08-25 08:48:20 +0800

    add Qualifier and more implenmentation

---
+++ src/com/murphy/RandomFortuneService.java
@@ -0,0 +1,11 @@
+package com.murphy;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RandomFortuneService implements FortuneService {
+    @Override
+    public String getFortune() {
+        return null;
+    }
+}
