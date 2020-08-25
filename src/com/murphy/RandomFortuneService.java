0dafa132769120978ab48af7316f50763e185963
Murphy Hsieh <murphyhsieh@leandev.io>
2020-08-25 08:57:24 +0800

    enhance random fortuen service

--- src/com/murphy/RandomFortuneService.java
+++ src/com/murphy/RandomFortuneService.java
@@ -2,10 +2,23 @@ package com.murphy;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Random;
+
 @Component
 public class RandomFortuneService implements FortuneService {
+
+    private String[] data = {
+            "1 First Fortune",
+            "2 Second Fortune",
+            "3 Third Fortune"
+    };
+
+    private Random myRandom = new Random();
+
     @Override
     public String getFortune() {
-        return null;
+        int index = myRandom.nextInt(data.length);
+
+        return data[index];
     }
 }
